616cae58e69e161f3980c13bf8005219b778dd84
Ahmad Ajmi <ahmad@ahmadajmi.com>
2015-08-18 10:52:45 +0200

    first commit

---
+++ app/components/Main.js
@@ -0,0 +1,17 @@
+'use strict';
+
+var React   = require('react');
+var hello   = require('./hello');
+
+var Hello = React.createClass({
+  render: function() {
+    return (
+      <h2>Hello {hello.sayHello('Ahmad :)')}</h2>
+      );
+  }
+})
+
+React.render(
+  <Hello />,
+  document.getElementById('content')
+);
---
+++ app/components/hello.js
@@ -0,0 +1,7 @@
+module.exports = {
+
+  sayHello: function(name) {
+    return name;
+  }
+
+};
